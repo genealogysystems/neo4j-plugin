e0937de2aafa901348fd302836248a28a1018b6b
John Clark <socrates37@gmail.com>
2013-11-01 12:58:59 -0600

    test now working

--- src/main/java/com/findarecord/neo4j/plugin/CollectionIndex.java
+++ src/main/java/com/findarecord/neo4j/plugin/CollectionIndex.java
@@ -2,8 +2,11 @@ package com.findarecord.neo4j.plugin;
 
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.server.plugins.*;
 import org.neo4j.tooling.GlobalGraphOperations;
+
+import java.util.List;
 
 @Description( "Find-A-Record Collections Index" )
 public class CollectionIndex extends ServerPlugin
@@ -13,6 +16,12 @@ public class CollectionIndex extends ServerPlugin
   @PluginTarget( GraphDatabaseService.class )
   public Iterable<Node> getAllNodes( @Source GraphDatabaseService graphDb )
   {
-    return GlobalGraphOperations.at(graphDb).getAllNodes();
+    Iterable<Node> result;
+
+    try(Transaction tx = graphDb.beginTx()) {
+      result = GlobalGraphOperations.at(graphDb).getAllNodes();
+      tx.success();
+    }
+    return result;
   }
 }
