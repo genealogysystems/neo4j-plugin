d8c989d64da1f449e5f5b09e4f04a3f742c50c02
John Clark <socrates37@gmail.com>
2013-11-01 12:05:39 -0600

    initial setup complete

---
+++ src/main/java/com/findarecord/neo4j/plugin/CollectionIndex.java
@@ -0,0 +1,21 @@
+package com.findarecord.neo4j.plugin;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.server.plugins.*;
+import org.neo4j.tooling.GlobalGraphOperations;
+
+/**
+ * Hello world!
+ *
+ */
+public class CollectionIndex extends ServerPlugin
+{
+  @Name( "get_all_nodes" )
+  @Description( "Get all nodes from the Neo4j graph database" )
+  @PluginTarget( GraphDatabaseService.class )
+  public Iterable<Node> getAllNodes( @Source GraphDatabaseService graphDb )
+  {
+    return GlobalGraphOperations.at(graphDb).getAllNodes();
+  }
+}
