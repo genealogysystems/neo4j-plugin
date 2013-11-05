9f2c7d1d12fd9fd39191bad5e2a082419b21172c
John Clark <socrates37@gmail.com>
2013-11-05 10:32:31 -0700

    non-working commit; need to refactor

--- src/main/java/com/findarecord/neo4j/plugin/CollectionIndex.java
+++ src/main/java/com/findarecord/neo4j/plugin/CollectionIndex.java
@@ -1,11 +1,11 @@
 package com.findarecord.neo4j.plugin;
 
-import com.vividsolutions.jts.geom.Geometry;
-import org.geotools.geojson.feature.FeatureJSON;
+import com.vividsolutions.jts.geom.*;
 import org.geotools.geojson.geom.GeometryJSON;
 import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.index.UniqueFactory;
 
+import java.awt.geom.Rectangle2D;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
@@ -58,13 +58,13 @@ public class CollectionIndex {
   public String indexPolygon(String geojson) {
 
     boolean noErrors = true;
+
     //validate/initialize polygon
-    int decimals = 15;
-    GeometryJSON gjson = new GeometryJSON(decimals);
+    Geometry geometry;
+    GeometryJSON gJSON = new GeometryJSON(15); //15 precision
     Reader reader = new StringReader(geojson);
     try {
-      Geometry geometry = gjson.read(reader);
-      //geometry.
+      geometry = gJSON.read(reader);
 
     } catch (IOException e) {
       return e.toString();
@@ -72,19 +72,146 @@ public class CollectionIndex {
 
 
     //turn polygon into boxes
-    ArrayList<Box> boxes = convertPolygonToBoxes();
+    String result = insertPolygon(geometry);
 
-    //insert boxes into graph
+
+    return result;
+  }
+
+  private UniqueFactory.UniqueEntity<Relationship> createRelationship(final Node start, final Node end, String indexableKey, final String indexableValue) {
+
+    UniqueFactory<Relationship> factory = new UniqueFactory.UniqueRelationshipFactory(graphDb, "relIdx") {
+      @Override
+      protected Relationship create(Map<String, Object> properties) {
+        Relationship r =  start.createRelationshipTo(end, DynamicRelationshipType.withName("idxContain"));
+        return r;
+      }
+    };
+
+    return factory.getOrCreateWithOutcome(indexableKey, indexableValue);
+  }
+
+  private Iterable<Box> convertDistanceToBoxes() {
+
+    return null;
+  }
+
+  private String insertPolygon(Geometry geometry) {
+    String ret = "";
+
+    //get first level of contains and find out intersects and covers
+    //todo move to insertBox
     try(Transaction tx = graphDb.beginTx()) {
-      for(Box box : boxes) {
-        if(!insertBox(box)) {
-          noErrors = false;
+
+      ArrayList<Geometry> topLevel = getFirstLevelGeometries();
+
+      ret += "number of topLevel: "+topLevel.size()+"  \n";
+
+      for(Geometry currentGeometry : topLevel) {
+
+        //ret += "comapring "+currentGeometry.toString()+"\n\n";
+        //if envelope is entirely inside geometry, add to contains
+        if(geometry.contains(currentGeometry)) {
+          //convert polygon to box and insert, then continue
+          continue;
+        }
+
+        //if envelope intersects geometry, recurse
+        if(geometry.intersects(currentGeometry)) {
+          //recurse on this geometry
+          ret += "found geom "+currentGeometry.toString();
         }
       }
       tx.success();
     }
 
-    return "";
+    return ret;
+  }
+
+  private ArrayList<Geometry> getFirstLevelGeometries() {
+    ArrayList<Geometry> geometries = new ArrayList<>();
+    int fromX = -18;
+    int toX = 18;
+    int fromY = 9;
+    int toY = -9;
+
+    for(int x=fromX;x<toX; x = x+1) {
+      for(int y=fromY;y>toY; y = y-1) {
+        Coordinate[] coords = new Coordinate[5];
+        coords[0] = new CustomCoordinate(x,y,10);
+        coords[1] = new CustomCoordinate(x+1,y,10);
+        coords[2] = new CustomCoordinate(x+1,y+1,10);
+        coords[3] = new CustomCoordinate(x,y+1,10);
+        coords[4] = new CustomCoordinate(x,y,10);
+        LinearRing ring = new GeometryFactory().createLinearRing(coords);
+        Geometry geom = new GeometryFactory().createPolygon(ring,null);
+        geometries.add(geom);
+      }
+    }
+
+    return geometries;
+  }
+
+
+  private String insertPolygonRecursive(Geometry geometry, int x, int y, int precision) {
+    //x and y should already be multiplied by 10, and precision divided by 10
+
+    String ret = "";
+
+    //base case compare precision and return early
+
+    //generate geometries
+    ArrayList<Geometry> topLevel = getNLevelGeometries(x,y,precision);
+
+    for(Geometry currentGeometry : topLevel) {
+
+      if(geometry.contains(currentGeometry)) {
+        //convert polygon to box and insert, then continue
+        CustomCoordinate coord = (CustomCoordinate)currentGeometry.getCoordinate();
+        insertBox(new Box(coord.boxX,coord.boxX, precision));
+        continue;
+      }
+
+      //if envelope intersects geometry, recurse
+      if(geometry.intersects(currentGeometry)) {
+        //recurse on this geometry
+        ret += "found geom "+currentGeometry.toString();
+      }
+    }
+
+    //check contains
+
+    //check intersects
+
+
+
+    return ret;
+  }
+
+  private ArrayList<Geometry> getNLevelGeometries(int topX, int topY, int precision) {
+    //x and y should already be multiplied by 10, and precision divided by 10
+
+    ArrayList<Geometry> geometries = new ArrayList<>();
+    int fromX = topX;
+    int toX = topX+10;
+    int fromY = topY;
+    int toY = topY+10;
+
+    for(int x=fromX;x<toX; x = x+1) {
+      for(int y=fromY;y>toY; y = y-1) {
+        Coordinate[] coords = new Coordinate[5];
+        coords[0] = new CustomCoordinate(x,y,precision);
+        coords[1] = new CustomCoordinate(x+1,y,precision);
+        coords[2] = new CustomCoordinate(x+1,y+1,precision);
+        coords[3] = new CustomCoordinate(x,y+1,precision);
+        coords[4] = new CustomCoordinate(x,y,precision);
+        LinearRing ring = new GeometryFactory().createLinearRing(coords);
+        Geometry geom = new GeometryFactory().createPolygon(ring,null);
+        geometries.add(geom);
+      }
+    }
+
+    return geometries;
   }
 
   /**
@@ -115,48 +242,6 @@ public class CollectionIndex {
     }
 
     return toNode;  //To change body of created methods use File | Settings | File Templates.
-  }
-
-  private UniqueFactory.UniqueEntity<Relationship> createRelationship(final Node start, final Node end, String indexableKey, final String indexableValue) {
-
-    UniqueFactory<Relationship> factory = new UniqueFactory.UniqueRelationshipFactory(graphDb, "relIdx") {
-      @Override
-      protected Relationship create(Map<String, Object> properties) {
-        Relationship r =  start.createRelationshipTo(end, DynamicRelationshipType.withName("idxContain"));
-        return r;
-      }
-    };
-
-    return factory.getOrCreateWithOutcome(indexableKey, indexableValue);
-  }
-
-  private Iterable<Box> convertDistanceToBoxes() {
-
-    return null;
-  }
-
-  private ArrayList<Box> convertPolygonToBoxes() {
-    ArrayList<Box> boxes = new ArrayList<Box>();
-
-    boxes.add(new Box("123.45","90.123", precision));
-
-    return boxes;
-  }
-
-
-  private void tmp() {
-
-    /*
-    //get index nodefactory
-    UniqueFactory<Node> factory =  getUniqueFactory(graphDb);
-
-    //test node creation
-    try ( Transaction tx = graphDb.beginTx() ) {
-      Node node = factory.getOrCreate( "name", "testing123" );
-      result.add(node.toString());
-      tx.success();
-    }
-    */
   }
 
 }
--- src/test/java/com/findarecord/neo4j/plugin/CollectionIndexTest.java
+++ src/test/java/com/findarecord/neo4j/plugin/CollectionIndexTest.java
@@ -4,10 +4,8 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+
+public class CollectionIndexTest
     extends TestCase
 {
     /**
@@ -15,7 +13,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public CollectionIndexTest(String testName)
     {
         super( testName );
     }
@@ -25,13 +23,13 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( CollectionIndexTest.class );
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    public void testCollectionIndex()
     {
         assertTrue( true );
     }
